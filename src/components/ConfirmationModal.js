f013f44aeb66b73c5e8261507e601dacb3fc8881
Ronald <ronaldmichael.barnachea.cics@ust.edu.ph>
2025-02-18 13:41:51 +0800

    Fixed CSS to meet brand design

--- src/components/ConfirmationModal.js
+++ src/components/ConfirmationModal.js
@@ -1,20 +1,46 @@
 import React from "react";
 import { Modal, Button } from "react-bootstrap";
+import "../styles/ConfirmationStyles.css";  
 
-const ConfirmationModal = ({ show, onHide, onConfirm, title, message }) => {
+const ConfirmationModal = ({ 
+  show, 
+  onHide, 
+  onConfirm, 
+  title, 
+  message, 
+  modalSize = "lg", 
+  titleClass = "default-modal-title", 
+  bodyClass = "default-modal-body",   
+  footerClass = "default-modal-footer", 
+  buttonClass = "default-modal-btn"  
+}) => {
   return (
-    <Modal show={show} onHide={onHide} centered>
-      <Modal.Header closeButton>
+    <Modal 
+      show={show} 
+      onHide={onHide} 
+      centered 
+      size={modalSize} 
+      className="custom-modal" 
+    >
+      <Modal.Header className={titleClass}>
         <Modal.Title>{title || "Confirm Action"}</Modal.Title>
       </Modal.Header>
-      <Modal.Body>
+      <Modal.Body className={bodyClass}>
         <p>{message || "Are you sure you want to proceed?"}</p>
       </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={onHide}>
+      <Modal.Footer className={footerClass}>
+        <Button 
+          variant="secondary" 
+          onClick={onHide} 
+          className={`${buttonClass} cancel-btn`} // Add custom cancel class
+          >
           Cancel
         </Button>
-        <Button variant="danger" onClick={onConfirm}>
+        <Button 
+          variant="danger" 
+          onClick={onConfirm} 
+          className={`${buttonClass} confirm-btn`} // Add custom confirm class
+          >
           Confirm
         </Button>
       </Modal.Footer>
--- src/components/PopUpModal.js
+++ src/components/PopUpModal.js
@@ -1,20 +1,41 @@
 import React from "react";
 import { Modal, Button } from "react-bootstrap";
+import "../styles/PopUpStyles.css";  
 
-const PopUpModal = ({ show, onHide, title, message }) => {
+const PopUpModal = ({ 
+  show, 
+  onHide, 
+  title, 
+  message, 
+  modalSize = "lg", 
+  titleClass = "default-modal-title", 
+  bodyClass = "default-modal-body",   
+  footerClass = "default-modal-footer", 
+  buttonClass = "default-modal-btn"  
+}) => {
   return (
-    <Modal show={show} onHide={onHide} centered>
-        <Modal.Header closeButton>
+    <Modal 
+      show={show} 
+      onHide={onHide} 
+      centered 
+      size={modalSize} 
+      className="custom-modal" 
+    >
+      <Modal.Header className={titleClass}>
         <Modal.Title>{title || "Confirm Action"}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
+      </Modal.Header>
+      <Modal.Body className={bodyClass}>
         <p>{message || "Are you sure you want to proceed?"}</p>
-        </Modal.Body>
-        <Modal.Footer>
-        <Button variant="secondary" onClick={onHide}>
-            Confirm
+      </Modal.Body>
+      <Modal.Footer className={footerClass}>
+        <Button 
+          variant="secondary" 
+          onClick={onHide} 
+          className={buttonClass}
+        >
+          Confirm
         </Button>
-        </Modal.Footer>
+      </Modal.Footer>
     </Modal>
   );
 };
